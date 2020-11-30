4ef5eb63c069f273b7e0c1bbb0c52235a12df238
Raymond <homefrontinvesting@gmail.com>
2020-11-30 11:38:37 -0700

    Initial Activity completed

---
+++ app/src/main/java/com/example/contactmanager/PersonAdapter.java
@@ -0,0 +1,31 @@
+package com.example.contactmanager;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class PersonAdapter extends BaseAdapter {
+    Activity mActivity;
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
